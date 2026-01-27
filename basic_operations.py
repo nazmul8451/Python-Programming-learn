6186f7d0a26c7fc571ff0f0fc94e8137bd817eb4
nazmul8451 <heytrading118@gmail.com>
2026-01-27 06:28:56 +0600

    add basic_operations.py

---
+++ basic_operations.py
@@ -0,0 +1,16 @@
+#math
+sum = 10 +5
+difference = 10-5
+modular = 10%3
+multiplication = 10*5
+division = 10/3
+
+print("Sum :",sum ,"Difference : ",difference, "Modular : ",modular , "Multiplication : ",multiplication , "Division : :",division, sep='\n')
+
+print("------------------Add Two String---------------")
+greeting = "Hello" + " " + "World"
+print("Ans Is: ",greeting)
+
+print("------------------Repeat String---------------")
+repeated_str = "Rimon " * 3 
+print("Ans Is: ",repeated_str)
