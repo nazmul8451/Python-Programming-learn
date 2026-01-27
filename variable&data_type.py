7da164e59716e7874b4d5394deb51a913a2e3ab7
nazmul8451 <heytrading118@gmail.com>
2026-01-27 06:14:54 +0600

    add varible and data type .py added

---
+++ variable&data_type.py
@@ -0,0 +1,35 @@
+#numbers
+age = 21
+price = 134.23
+#strings
+name = 'Rimon islam'
+country = 'Bangladesh'
+#boolean
+is_student = True
+has_graduateed = False
+
+#List 
+fruits = ['apple','banana','mango','orange']
+numbers = [1,3,4,5,66,74,23]
+
+numbers.append(12) # append ekta list e notun element add kore
+numbers.remove(12) # remove ekta list theke element remove kore
+numbers.insert(1,99)
+numbers.sort() # sort list in ascending order
+
+print("Total List : ",numbers)
+
+#Tuple 
+coardinates = (10.4,23.2)
+print(coardinates)
+
+
+#python ->dictionary  Dart-> Map
+person = {
+    "name" : "Rimon islam",
+    "age" : 21,
+    "country" : "Bangladesh"
+    }
+print(person)
+print(person['age'])
+print(person['country'])
