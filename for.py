24045402dd52fecebfe40ba181d96d4d18e28e9e
nazmul8451 <heytrading118@gmail.com>
2026-02-02 22:55:35 +0600

    add for.py updated

--- for.py
+++ for.py
@@ -2,7 +2,15 @@ numbers = [1,32,13,14,12,41,32]
 
 sum = 0 
 
-for num in numbers:
-    print(num)
-    sum += num
-print("Total Sum is: ", sum)    
+# for num in numbers:
+#     print(num)
+#     sum += num
+# print("Total Sum is: ", sum)    
+
+# for i in range(1,11):
+#     if(i==5 or i == 7 or i == 9):
+#         continue
+#     if(i == 1 ):
+#         print("starting value ",i )
+#     print("value is: ",i)
+
