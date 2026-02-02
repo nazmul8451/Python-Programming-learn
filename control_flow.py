09b1783c27f16e9fae8edd726744ba768624e2e2
nazmul8451 <heytrading118@gmail.com>
2026-02-02 21:51:34 +0600

    add for.py

---
+++ control_flow.py
@@ -0,0 +1,12 @@
+
+# Control Flow Example
+age =  21
+if age >= 20 and age <= 25:
+    print('Adult')
+elif age >= 13 and age < 18:
+    print("Teenager")
+
+else: 
+    print("mara khaw")
+
+    print(age)
---
+++ for.py
@@ -0,0 +1,8 @@
+numbers = [1,32,13,14,12,41,32]
+
+sum = 0 
+
+for num in numbers:
+    print(num)
+    sum += num
+print("Total Sum is: ", sum)    
