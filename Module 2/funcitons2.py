e81f040105f21ee1989747588cc3ae982978cdda
nazmul8451 <heytrading118@gmail.com>
2026-02-03 19:04:35 +0600

    add new module two file

---
+++ Module 2/funcitons2.py
@@ -0,0 +1,6 @@
+def two_sum(a,b):
+    sum = a +b
+    return sum
+
+total = two_sum(4,6)
+print("Two sum is : ",total)
---
+++ Module 2/functions.py
@@ -0,0 +1,6 @@
+
+def double_it(num):
+    result = num*2 ;
+    print("doubel result is : ",result * result)
+double_it(53)
+
