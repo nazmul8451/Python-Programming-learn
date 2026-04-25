450db7449318795d215dfae355a2d5a35e39a1e5
nazmul8451 <heytrading118@gmail.com>
2026-04-25 07:35:20 +0600

    add all

---
+++ Python/learn/global_variable.py
@@ -0,0 +1,18 @@
+#that is the global varibale///
+w = "Tasfia Sumiya Ritu"
+
+global x
+def my_wife():
+
+    w= "Ritu islam"
+
+    print("My wife name is ", w)
+
+my_wife()
+my_ex()
+
+def my_ex():
+
+    x = "Priya"
+
+    print("My Ex name is ",x)
--- Python/learn/python_variable.py
+++ Python/learn/python_variable.py
@@ -17,5 +17,35 @@ z= float(4.5)
 
 print("str:",x,"int:",y,"float:",z)"""
 
-x =5 
-print(type(x))
+""""x =5 
+print(type(x))"""
+
+
+x,y,z = 5,10,15
+print("x-",x)
+print("y-",y)
+print("z-",z)
+print("---------------------------------------")
+
+x=y=z = "Orrange"
+
+print("x",x)
+print("y",y)
+print("z",z)
+print("---------------------------------------")
+
+fruits = ["Rimon","Bayzid","Sakil"]
+
+x,y,z = fruits
+print("x-",x)
+print("y-",y)
+print("z-",z)
+
+print("---------------------------------------")
+
+print("PythonMoja")
+print("---------------------------------------")
+
+x = str(5)
+y = " John"
+print(x + y)
